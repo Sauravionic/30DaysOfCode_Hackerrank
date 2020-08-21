65b8e0a650b1e1741eabe0f837b2c05104a2dac6
Saurav <srivastava2000saurav@gmail.com>
2020-08-21 12:37:55 +0530

    Create day_1.java

---
+++ Day 1/day_1.java
@@ -0,0 +1,26 @@
+  /* Declare second integer, double, and String variables. */
+
+      int second; double sec; String str;
+
+        /* Read and save an integer, double, and String to your variables.*/
+
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+
+        second = scan.nextInt();
+        sec = scan.nextDouble();
+        scan.nextLine();
+        str = scan.nextLine();
+
+
+        /* Print the sum of both integer variables on a new line. */
+
+        System.out.println(i+second);
+        System.out.println(d + sec);
+        System.out.println(s + str);
+
+        /* Print the sum of the double variables on a new line. */
+		
+        
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
