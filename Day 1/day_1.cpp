8b3250e51f7e2aaadf15835fd0b22d81314205fa
Saurav <srivastava2000saurav@gmail.com>
2020-08-21 12:37:51 +0530

    Create day_1.cpp

---
+++ Day 1/day_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+  
+  int second;
+  double sec;
+  String str;
+  cin >> second;
+  cin >> sec;
+  cin >> str;
+  cout << i + second << endl;   // i is already defined by the compiler
+  cout << d + sec << endl;      // d is already defined by the compiler
+  cout << s + str << endl;      // s is already defined by the compiler
+
+   return 0;
+   }
