e134f9a3ee12d558a46977f399d2dc81a5b7d8fa
Saurav <srivastava2000saurav@gmail.com>
2020-08-21 13:24:44 +0530

    Create day_5.cpp

---
+++ Day 5/day_5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+  
+  int N;
+
+  cin >> N;
+
+  for(int i = 1; i <= 10; i++)
+        {
+            cout << n << " x " << i << " = " << n*i << endl;
+        }
+
+  return 0
+    }
